e26d3c50b055f4199a2c2ec81fdef6803b0da163
Julius Adebowale Gabriel <juadebgabriel@gmail.com>
2026-03-13 23:05:52 +0100

    restart

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -8,6 +8,24 @@ import { ConfigService } from '@config/config.service';
       useFactory: (configService: ConfigService) => ({
         uri: configService.database.uri,
         autoIndex: !configService.isProd,
+        serverSelectionTimeoutMS: 10000,
+        connectTimeoutMS: 10000,
+        socketTimeoutMS: 20000,
+        connectionFactory: (connection) => {
+          connection.on('connected', () => {
+            // eslint-disable-next-line no-console
+            console.log('MongoDB connected');
+          });
+          connection.on('error', (error) => {
+            // eslint-disable-next-line no-console
+            console.error('MongoDB connection error:', error?.message ?? error);
+          });
+          connection.on('disconnected', () => {
+            // eslint-disable-next-line no-console
+            console.warn('MongoDB disconnected');
+          });
+          return connection;
+        },
       }),
       inject: [ConfigService],
     }),
